3a94887b56028550edad47718e3ead1e63b090f8
Chi Le <lehuechi04@gmail.com>
2023-08-10 01:26:22 +0700

    Add rating system

--- README.md
+++ README.md
@@ -2,8 +2,24 @@
 The Odin Project - 8th project
 
 An online library to store books.
+Including rating system and read/not finished feature.
+
+## Usage
+
+[LIVE DEMO](https://lhchi04.github.io/my-library/)
 
 ## Credits
 
-[Checkbox tutorial](https://moderncss.dev/pure-css-custom-checkbox-style/)
+[Customized checkbox tutorial](https://moderncss.dev/pure-css-custom-checkbox-style/)
+
 [Toggle button tutorial](https://designyff.com/code/on_off_toggle_text)
+
+[Star rating form tutorial](https://codeconvey.com/feedback-form-in-html/?expand_article=1)
+
+[Free Star icon](https://iconscout.com/icons/star) by [Typicons](https://iconscout.com/contributors/typicons-1)
+
+[Free Star icon](https://iconscout.com/icons/star) by [Sriti Chamola](https://iconscout.com/contributors/quirkyillustrator)
+
+## Status
+
+In progress
--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 let myLibrary = []
 
-function Book(title, author, pages, finished) {
+function Book(title, author, pages, finished, rating) {
   this.title = title
   this.author = author
   this.pages = pages
   this.finished = finished
+  this.rating = rating
   this.titleInfo = function() {
     return title;
   }
@@ -16,40 +17,50 @@ function Book(title, author, pages, finished) {
       return 'Not finished'
     return 'Read'
   }
+  this.ratingInfo = function() {
+    if (rating === -1) return -1
+    return parseInt(rating)
+  }
 }
 
 function addToLibrary() {
   let i = myLibrary.length-1;
-  let book = new Book(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].finished);
+  let book = new Book(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].finished, myLibrary[i].rating);
   return book;
 }
 
 function printPreview(book, position) {
   if (position === 0) return book.titleInfo();
   else if (position === 1) return book.authorInfo();
-  return book.finishedInfo();
+  else if (position === 2) return book.finishedInfo();
+  return book.ratingInfo();
 }
 
 let read;
-
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let pages = document.getElementById('pages');
+let finished = document.getElementById('finished');
+let allRating = Array.from(document.querySelectorAll('input[type=radio]'));
 const form = document.querySelector('form');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  let title = document.getElementById('title');
-  let author = document.getElementById('author');
-  let pages = document.getElementById('pages');
-  let finished = document.getElementById('finished');
+
   let newBook = {
     title: title.value,
     author: author.value,
     pages: pages.value,
     finished: finished.checked,
+    rating: value,
   }
   myLibrary.push(newBook);
   let myNewBook = addToLibrary();
+
   // Add book preview
   const bookPreview = document.createElement('div');
   bookPreview.classList.add('preview');
+
   // Add book preview content
   const content = document.createElement('p');
   content.classList.add('content');
@@ -59,26 +70,35 @@ form.addEventListener('submit', (e) => {
   content.appendChild(authorContent);
   const finishedContent = document.createElement('h4');
   content.appendChild(finishedContent);
-  titleContent.textContent = printPreview(myNewBook,0).toString();
-  authorContent.textContent = printPreview(myNewBook,1).toString();
-  finishedContent.textContent = printPreview(myNewBook,2).toString();
+  const ratingContent = document.createElement('div');
+  ratingContent.classList.add('rating-display');
+  content.appendChild(ratingContent);
+  titleContent.textContent = printPreview(myNewBook, 0);
+  authorContent.textContent = printPreview(myNewBook, 1);
+  finishedContent.textContent = printPreview(myNewBook, 2);
+  displayStar(ratingContent, printPreview(myNewBook, 3));
+
   // Add setting section
   const setting = document.createElement('div');
   setting.classList.add('setting');
+
   // Add button-container
   const buttonContainer = document.createElement('div');
   buttonContainer.classList.add('button-container');
   setting.appendChild(buttonContainer);
+
   // Add view button
   const view = document.createElement('button');
   view.textContent = 'View';
   view.classList.add('view');
   buttonContainer.appendChild(view);
+
   // Add delete button
   const del = document.createElement('button');
   del.textContent = 'Delete';
   del.classList.add('delete');
   buttonContainer.appendChild(del);
+
   // Add toggle button
   const toggleContainer = document.createElement('div');
   toggleContainer.classList.add('toggle-container');
@@ -103,9 +123,9 @@ form.addEventListener('submit', (e) => {
   }
   toggleContainer.appendChild(toggleText);
   buttonContainer.appendChild(toggleContainer);
-  // Add content and setting to book preview
   bookPreview.appendChild(content);
   bookPreview.appendChild(setting);
+
   // Hover over preview to show setting
   bookPreview.addEventListener('mouseover', () => {
     bookPreview.lastChild.style.display = "block";
@@ -114,6 +134,7 @@ form.addEventListener('submit', (e) => {
     bookPreview.lastChild.style.display = "none";
   });
   document.querySelector('.books').appendChild(bookPreview);
+
   // Toggle the read button
   const index = myLibrary.indexOf(newBook);
   toggle.addEventListener('click', () => {
@@ -130,20 +151,57 @@ form.addEventListener('submit', (e) => {
       finishedContent.textContent = 'Not finished';
     }
   });
-  // View details of the book
-  // view.addEventListener('click', () => {
-  //   bookPreview.classList.add('viewPreview');
-  // });
+  
   // Delete the book preview
-  del.addEventListener('click', () => {
-    myLibrary.splice(index, 1);
-    del.parentElement.parentElement.parentElement.remove();
-  });
-  title.value = '';
-  author.value = '';
-  pages.value = '';
-  finished.checked = false;
+  del.addEventListener('click', () => deleteBook(del,index));
+  showEmpty();
+  resetForm();
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+  showEmpty();
+});
+
+// Check the rating
+let value = -1;
+function starValue() {
+  allRating.forEach(star => {
+    if (star.checked) {
+      value = star.value;
+    }
+  })
+  return value;
+}
+
+function displayStar(ratingContent, num) {
+  for (let i=0; i<5; i++) {
+    let star = document.createElement('div');
+    ratingContent.appendChild(star);
+  }
+  if (num === -1) {}
+  else {
+    for (let i=0; i<num; i++) {
+      let child = ratingContent.children[i];
+      child.classList.add('star-display');
+    }
+  }
+  return ratingContent;
+}
+
+function deleteBook(e, index) {
+  myLibrary.splice(index, 1);
+  e.parentElement.parentElement.parentElement.remove();
+  showEmpty();
+}
+
+function showEmpty() {
+  if (myLibrary.length === 0) {
+    document.querySelector('.empty').style.display = "block";
+  }
+  else {
+    document.querySelector('.empty').style.display = "none";
+  }
+}
 
 function openForm() {
   document.querySelector('.form-container').style.cssText = "opacity: 1; top: 50%";
@@ -151,5 +209,14 @@ function openForm() {
 
 function closeForm() {
   document.querySelector('.form-container').style.cssText = "opacity: 0; top: -400px;";
+}
+
+function resetForm() {
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  finished.checked = false;
+  if (value !== -1)
+    document.querySelector('input[name="rating"]:checked').checked = false;
 }
 
